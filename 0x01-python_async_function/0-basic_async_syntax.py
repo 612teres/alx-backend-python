a6defa66ca7f4dd04e973cd71d04da0aa05eb392
612teres <612teres@gmail.com>
2024-10-14 16:13:58 +0300

    concurrent coroutines

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,6 +4,6 @@ import random
 
 async def wait_random(max_delay: int = 10) -> float:
     '''Simulates a delay of random duration up to max_delay seconds.'''
-    delay = random.randint(0, max_delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
